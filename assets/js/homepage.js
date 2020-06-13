37136f1ca76a9cf45fdfb52301bdc6a93cb53c03
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-13 11:24:56 -0600

    getting monster cards working

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,9 +1,11 @@
 var searchEl = document.getElementById('search-button');
 var searchHistory = JSON.parse(localStorage.getItem('monster')) || [];
 var inputEl = document.getElementById('monster-input');
-var monsterContainerEl = document.getElementById('monster-container')
+var monsterContainerEl = document.getElementById('monster-container')    
+var queryURL = 'https://www.dnd5eapi.co/api/monsters/';
 
-//initialize search for given city name
+
+//initialize search for given monster name
 searchEl.addEventListener('click',function() {
     var searchTerm = inputEl.value;
     getMonster(searchTerm);
@@ -12,21 +14,49 @@ searchEl.addEventListener('click',function() {
 });
 
 function getMonster(name) {
-    var queryURL = 'https://www.dnd5eapi.co/api/monsters/' + name;
-    axios.get(queryURL)
+    axios.get(queryURL + name)
     .then (function(response) {
-        var monsterEl = document.createElement ('ul');
-        monsterEl.classList = 'card list-item';
-        monsterEl.setAttribute ('id', 'monster-container');
+        var monsterCardEl = document.createElement ('div');
+        monsterCardEl.classList = ('card');
+        monsterCardEl.setAttribute ('id', 'monster-container');
 
-        var monsterName = document.createElement('li');
+        var monsterContent = document.createElement ('div');
+        monsterContent.classList = ('card-content');
+        monsterCardEl.appendChild(monsterContent);
+
+        var monsterName = document.createElement('p');
+        monsterName.classList = ('card-title');
         monsterName.innerText = 'Name: ' + response.data.name;
-        monsterEl.appendChild(monsterName);
+        monsterCardEl.appendChild(monsterName);
 
-        var monsterHP = document.createElement('li');
-        monsterHP.innerText = 'HP: ' + response.data.hit_points;
-        monsterEl.appendChild(monsterHP);
+        var monsterHP = document.createElement('a');
+        monsterHP.classList = ('dropdown-button btn');
+        monsterHP.setAttribute ('href','#');
+        // monsterHP.setAttribute ('data-target', 'dropdown1');
+        monsterHP.setAttribute ('data-activates', 'dropdown');
+        monsterHP.innerText = 'HP';
+        monsterCardEl.appendChild(monsterHP);
 
-        monsterContainerEl.appendChild(monsterEl);
+        var hpDropdown = document.createElement('ul');
+        hpDropdown.classList = ('dropdown-content');
+        hpDropdown.setAttribute = ('id', 'dropdown');
+        monsterCardEl.appendChild(hpDropdown);
+
+        var hpContent = document.createElement('li');
+        hpDropdown.appendChild(hpContent);
+
+        var hpAnchor = document.createElement('a');
+        hpAnchor.setAttribute ('href', '#');
+        hpAnchor.innerText = response.data.hit_points;
+        hpContent.appendChild(hpAnchor);
+
+        var monsterStats = document.createElement('button');
+        monsterStats.innerText = 'STATS';
+        monsterCardEl.appendChild(monsterStats);
+
+        monsterContainerEl.appendChild(monsterCardEl);
     });
-}   
+} 
+
+
+
