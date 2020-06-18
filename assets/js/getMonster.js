b343f18e7e2ea01fd0dc4eb511ed213f29161eac
John Nielsen <johnhayesnielsen@gmail.com>
2020-06-18 12:06:03 -0600

    added jake code for player modal and card

--- assets/js/getMonster.js
+++ assets/js/getMonster.js
@@ -1,62 +1,9 @@
 var searchEl = document.getElementById('search-button');
 var searchHistory = JSON.parse(localStorage.getItem('monster')) || [];
 var inputEl = document.getElementById('monster-input');
-var monsterContainerEl = document.getElementById('monsterList-container')
+var monsterContainerEl = document.getElementById('monsterColumn')
 var queryURL = 'https://www.dnd5eapi.co/api/monsters/';
 console.log(searchHistory);
-
-$("#addplayerSubmit").click(function (event) {
-    event.preventDefault();
-
-    var playerName = $("#playerName").val();
-    var playerClass = $("#playerClass").val();
-    var playerRace = $("#playerRace").val();
-    var healthPoints = $("#healthPoints").val();
-    var strength = $("#strength").val();
-    var dexterity = $("#dexterity").val();
-    var intelligence = $("#intelligence").val();
-    var wisdom = $("#wisdom").val();
-    var constitution = $("#constitution").val();
-    var charisma = $("#charisma").val();
-    var weaponsOne = $("#weaponsOne").val();
-    var weaponsTwo = $("#weaponsTwo").val();
-    var weaponsThree = $("#weaponsThree").val();
-    var weaponsFour = $("#weaponsFour").val();
-    var weaponsFive = $("#weaponsFive").val();
-    var weaponsSix = $("#weaponsSix").val();
-    var weaponsSeven = $("#weaponsSeven").val();
-    var weaponsEight = $("#weaponsEight").val();
-    var spellOne = $("#spellOne").val();
-    var spellTwo = $("#spellTwo").val();
-    var spellThree = $("#spellThree").val();
-    var spellFour = $("#spellFour").val();
-    var statusEffectOne = $("#statusEffectOne").val();
-    var statusEffectTwo = $("#statusEffectTwo").val();
-    var statusEffectThree = $("#statusEffectThree").val();
-
-    var loadplayerInfo = function () {
-        $("#playerInfo,#playerInfotwo").html("Player Name: " + "<u><b>" + playerName + "</u></b>" + "    Player Class: " + "<u><b>" + playerClass + "</u></b>" + "Player Race: " + "<u><b>" + playerRace + "       " + "</u></b>" + "         Player Health: " + "<u><b>" + healthPoints);
-
-    };
-    loadplayerInfo();
-
-
-    var arrayForm = [playerName, playerClass, playerRace, healthPoints, strength, dexterity, intelligence, wisdom, constitution, charisma, weaponsOne, weaponsTwo, weaponsThree, weaponsFour, weaponsFive, weaponsSix, weaponsSeven, weaponsEight, spellOne, spellTwo, spellThree, spellFour, statusEffectOne, statusEffectTwo, statusEffectThree, statusEffectFour];
-
-    // var texttoEnter = arrayForm;
-    var texttoEnterJSON = JSON.stringify(arrayForm);
-    localStorage.setItem("addplayerSubmitLocalStorage", texttoEnterJSON);
-    loadText();
-});
-
-
-// recall local storage
-var loadText = function () {
-    texttoEnter = JSON.parse(localStorage.getItem("addplayerSubmitLocalStorage"));
-    $("#inbox").html(texttoEnter[4]);
-};
-
-
 
 //initialize search for given monster name
 searchEl.addEventListener('click', function () {
@@ -69,6 +16,7 @@ searchEl.addEventListener('click', function () {
 function getMonster(name) {
     axios.get(queryURL + name)
         .then(function (response) {
+            var queryURL = 'https://www.dnd5eapi.co/api/monsters/';
             var monsterCardEl = document.createElement('div');
             monsterCardEl.classList = ('card monsterList-container');
 
@@ -131,33 +79,4 @@ function getMonster(name) {
         });
 }
 
-// generates content for the modal
-var modalHandler = function (playerEl) {
-    var playerContainerEl = $("#monsterList-container") // #monsterList-container needs to be switched to player
-    var playerList = playerContainerEl.find(".monsterName") || []; // .monsterName needs to be switched to player name
-    console.log(playerList);
-
-    // takes each player entered and makes their name into a button in the modal
-    playerList.each(function () {
-        var playerName = $(this).text().split(":")[1].trim();
-        var modalButtonEl = $("<a>").addClass("waves-effect waves-light btn").text(playerName);
-        var modalContentEl = $(".modal-content");
-        modalContentEl.append(modalButtonEl); // buttons don't generate with spacing... idk why.
-    });
-}
-
-$(document).ready(function () {
-    $('.tooltipped').tooltip();
-});
-
-$(document).ready(function () {
-    $('.modal').modal();
-});
-
-$("#startBtn").click(function () {
-    modalHandler($(this).html())
-});
-
-$(document).ready(function () {
-    $('.tabs').tabs();
-});
+// export { getMonster };
