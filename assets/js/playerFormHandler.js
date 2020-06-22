7da497e2ddc03ec7824a9e2fb3667d26e3821168
Jacob Corum <cor15031@byui.edu>
2020-06-21 20:29:06 -0600

    player cards persistent

--- assets/js/playerFormHandler.js
+++ assets/js/playerFormHandler.js
@@ -1,6 +1,7 @@
 $("#addplayerSubmit").click(function () {
     playerFormHandler(event);
 });
+
 var playerContainerEl = $("<ul>").addClass("row s12 contain").attr("id", "playerContainer");
 var monsterContainerEl = $("<ul>").addClass("row s12 contain").attr("id", "monsterContainer");
 var battleContainerEl = $("<ul>").addClass("row s12 contain").attr("id", "battleContainer");
@@ -14,8 +15,66 @@ $(".contain").sortable({
     helper: "clone"
 })
 
-var playerFormHandler = function (event) {
-    event.preventDefault();
+var createPlayerCard = function (playerData) {
+    if (playerData.isplayer) {
+        var rowEl = $("<li>").addClass("row draggable");
+        var colEl = $("<div>").addClass("col s12");
+        var cardEl = $("<div>").addClass("card player-card s12").attr('draggable', 'true');
+        var cardContentEl = $("<div>").addClass("card-content white-text");
+        var cardTitle = $("<span>").addClass("card-title").text(playerData.playerName).attr("id", playerData.playerName);
+        var cardRaceClass = $("<p>").text(`${playerData.playerRace} ${playerData.playerClass}`);
+        var cardHpInput = $('<input>').addClass('center').attr('placeholder', 'HP: ' + playerData.healthPoints);
+        var cardAction = $("<div>").addClass("card-action");
+        var cardHealthPoints = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", playerData.healthPoints).text("HP");
+        var cardSpellButton = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", `${playerData.spellOne}, ${playerData.spellTwo}, ${playerData.spellThree}, ${playerData.spellFour}`).text("Spells");
+        var cardStatsButton = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", `${playerData.strength}, ${playerData.dexterity}, ${playerData.intelligence}, ${playerData.wisdom}, ${playerData.constitution}, ${playerData.charisma}`).text("Stats");
+        var cardWeapon = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", playerData.weapon).text("Weapon");
+        var cardStatusEffect = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", playerData.statusEffect).text("Status");
+
+        cardAction.append(cardHealthPoints, cardSpellButton, cardStatsButton, cardWeapon, cardStatusEffect);
+        cardContentEl.append(cardTitle, cardRaceClass, cardHpInput, cardAction);
+        cardEl.append(cardContentEl);
+        colEl.append(cardEl);
+        rowEl.append(colEl);
+        playerContainerEl.append(rowEl);
+
+        $("<button>").addClass("btn battleBtn").text("Battle!").appendTo(cardContentEl);
+    }
+}
+
+var createOldPlayerCard = function () {
+    persistencyData = JSON.parse(localStorage.getItem("persistencyData"));
+    if (!persistencyData) {
+        persistencyData = [];
+    }
+
+    persistencyData.forEach(player => {
+        if (player.isPlayer) {
+            playerData = {
+                "playerName": player.playerName,
+                "playerClass": player.playerClass,
+                "playerRace": player.playerRace,
+                "healthPoints": player.healthPoints,
+                "strength": player.strength,
+                "dexterity": player.dexterity,
+                "intelligence": player.intelligence,
+                "wisdom": player.wisdom,
+                "constitution": player.constitution,
+                "charisma": player.charisma,
+                "weapon": player.weapon,
+                "spellOne": player.spellOne,
+                "spellTwo": player.spellTwo,
+                "spellThree": player.spellThree,
+                "spellFour": player.spellFour,
+                "statusEffect": player.statusEffect,
+                "isplayer": true
+            }
+            createPlayerCard(playerData);
+        }
+    })
+}
+
+var createNewPlayerCard = function () {
     var playerName = $("#playerName").val();
     var playerClass = $("#playerClass").text();
     var playerRace = $("#playerRace").text();
@@ -33,28 +92,37 @@ var playerFormHandler = function (event) {
     var spellFour = $("#spellFour").val();
     var statusEffect = $("#playerStatus").text();
 
-    var rowEl = $("<li>").addClass("row draggable");
-    var colEl = $("<div>").addClass("col s12");
-    var cardEl = $("<div>").addClass("card player-card s12").attr('draggable', 'true');
-    var cardContentEl = $("<div>").addClass("card-content white-text");
-    var cardTitle = $("<span>").addClass("card-title").text(playerName).attr("id", playerName);
-    var cardRaceClass = $("<p>").text(`${playerRace} ${playerClass}`);
-    var cardHpInput = $('<input>').addClass('center').attr('placeholder', 'HP: ' + healthPoints);
-    var cardAction = $("<div>").addClass("card-action");
-    var cardHealthPoints = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", healthPoints).text("HP");
-    var cardSpellButton = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", `${spellOne}, ${spellTwo}, ${spellThree}, ${spellFour}`).text("Spells");
-    var cardStatsButton = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", `${strength}, ${dexterity}, ${intelligence}, ${wisdom}, ${constitution}, ${charisma}`).text("Stats");
-    var cardWeapon = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", weapon).text("Weapon");
-    var cardStatusEffect = $("<a>").addClass("btn tooltipped").attr("data-position", "bottom").attr("data-tooltip", statusEffect).text("Status");
+    var playerData = {
+        "playerName": playerName,
+        "playerClass": playerClass,
+        "playerRace": playerRace,
+        "healthPoints": healthPoints,
+        "strength": strength,
+        "dexterity": dexterity,
+        "intelligence": intelligence,
+        "wisdom": wisdom,
+        "constitution": constitution,
+        "charisma": charisma,
+        "weapon": weapon,
+        "spellOne": spellOne,
+        "spellTwo": spellTwo,
+        "spellThree": spellThree,
+        "spellFour": spellFour,
+        "statusEffect": statusEffect,
+        "isPlayer": true
+    }
 
-    cardAction.append(cardHealthPoints, cardSpellButton, cardStatsButton, cardWeapon, cardStatusEffect);
-    cardContentEl.append(cardTitle, cardRaceClass, cardHpInput, cardAction);
-    cardEl.append(cardContentEl);
-    colEl.append(cardEl);
-    rowEl.append(colEl);
-    playerContainerEl.append(rowEl);
+    createPlayerCard(playerData);
 
-    battleBtnHandler(cardContentEl);
+    persistencyData.push(playerData);
+
+    localStorage.setItem("persistencyData", JSON.stringify(persistencyData));
+}
+
+var playerFormHandler = function (event) {
+    event.preventDefault();
+    debugger;
+    createNewPlayerCard()
 
     M.Dropdown.init(playerFormHandler);
 
@@ -73,7 +141,6 @@ var playerFormHandler = function (event) {
     $(document).ready(function () {
         $('.tabs').tabs();
     });
-
 
     $('.dropdown-trigger').dropdown();
 
@@ -109,7 +176,4 @@ var playerFormHandler = function (event) {
     })
 }
 
-var battleBtnHandler = function (card) {
-    $("<button>").addClass("btn battleBtn").text("Battle!").appendTo(card);
-}
-
+createOldPlayerCard();
